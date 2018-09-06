d8345513cb5f3ce5ff2b4396d149a1530d60227f
KarelAditya <43022651+KarelAditya@users.noreply.github.com>
2018-09-06 10:04:30 +0700

    Add files via upload

---
+++ Karel.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author asus
+ */
+public class Karel {
+    
+/**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int m=5;
+        for (int n=1;n<=5;n++){
+            for(int o=1;o<=n;o++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
